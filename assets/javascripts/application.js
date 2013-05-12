e4e73ed2f7ce072f11409ac675c852f538f67995
Sean Connelly <sean@seanconnelly.me>
2013-05-12 14:35:11 -0700

    Updating bootstrap and other assets

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,60 @@
 /*jslint browser: true*/
 /*global $, jQuery, moment*/
 // This is a manifest file that'll be compiled into application.min.js
+(function () {
+    var hulu = {
+        init: function () {
+            hulu.registerEvents();
+        },
+
+        registerEvents: function () {
+            // Handles all form submissions
+            $('body').on('submit', 'form', function (e) {
+                e.preventDefault();
+
+                var responseContainer = $(this).siblings('.response_container'),
+                    codeContainer = $(this).siblings('.code_snippet'),
+                    codeTemplate = $(codeContainer.data('template')).html(),
+                    selectFields = $(this).children('select, input'),
+                    requestParams = {},
+                    templateData = {
+                        method: codeContainer.data('method'),
+                        params: []
+                    };
+
+                // Retrieve all form options
+                for (var i = 0;i < selectFields.length;i += 1) {
+                    var key = $(selectFields[i]).attr('name'),
+                        value = $(selectFields[i]).val();
+
+                    // Only push parameters if the value isn't empty
+                    if (value !== '') {
+                        requestParams[key] = value;
+                        templateData.params.push({key: key, value: value});
+                    }
+                }
+
+                // Render the PHP code snippet
+                codeContainer.html(Mustache.to_html(codeTemplate, templateData));
+
+                // Make the request to api.php!
+                $.ajax({
+                    url: $(this).attr('action'),
+                    type: 'GET',
+                    data: requestParams,
+                    success: function (response) {
+                        // Render the response and display it in the response container
+                        console.log(response)
+                        responseContainer.text(response);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+            });
+        }
+    };
+
+    $(document).ready(hulu.init);
+}());
