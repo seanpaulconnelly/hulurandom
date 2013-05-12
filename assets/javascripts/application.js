87eadc225524ff755c400b0ae5c9ee7c8a63805f
Sean Connelly <sean@seanconnelly.me>
2013-05-12 14:15:35 -0700

    Getting Bootstrap, jQuery, and an index file in there

---
+++ assets/javascripts/application.js
@@ -0,0 +1,3 @@
+/*jslint browser: true*/
+/*global $, jQuery, moment*/
+// This is a manifest file that'll be compiled into application.min.js
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HuluRandom - Randomly pick something to watch on Hulu</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='assets/application.css' rel='stylesheet' type='text/css'>
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    	<!--[if lt IE 9]>
+      	<script src="assets/javascripts/html5shiv.js"></script>
+    	<![endif]-->
+	</head>
+	<body>
+		<h1><i class="icon-search"></i>HuluRandom</h1>
+		<h2>Randomly pick something to watch on Hulu</h2>
+		<script src="assets/jquery.min.js" type='text/javascript'></script>
+		<script src='assets/application.min.js' type='text/javascript'></script>
+	</body>
+</html>
