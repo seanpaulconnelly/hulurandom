adcc0de87c98cd54dc3a90458c982453e00dddbf
Sean Connelly <sean@seanconnelly.me>
2013-05-12 21:32:45 -0700

    Use-it-by-tonight commit

--- api.php
+++ api.php
@@ -9,19 +9,26 @@
 
             // Call the method!
             $videos = $hulu->getVideos($_GET);
-            
+
             foreach ($videos as $video) {
                 $videoId = $video->video_id;
+                $videoTitle = $video->title;
                 $largeThumb = $video->thumbnail_url_16x9_medium;
                 $vidDuration = $video->duration;
                 $vidDescription = $video->description;
                 $eid = $video->eid;
                 if (!empty($video)) {
-                    echo '<a target="_blank" href="http://hulu.com/watch/' . $videoId . '">Watch on Hulu</a><br/>';
-                    echo '<img src="' . $largeThumb . '"/><br/>';
-                    echo gmdate("H:i:s", intval($vidDuration));
-                    echo '<iframe src="http://www.hulu.com/embed.html?eid=' . $eid . '" width="640px" height="360px" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>';
-                    echo $vidDescription;
+                    echo '<div class="container"><div class="row"><div class="span3 sidebar">';
+                    echo '<h3>' . $videoTitle . '</h3>';
+                    echo '<img src="' . $largeThumb . '"/>';
+                    echo '<h4>Length: ' . gmdate("H:i", intval($vidDuration)) . '</h4>';
+                    echo '<h4>Description</h4>';
+                    echo '<p>' . $vidDescription . '</p>';
+                    echo '<p class="hidden-phone"><a target="_blank" href="http://hulu.com/watch/' . $videoId . '">Watch on Hulu</a></p>';
+                    echo '</div><div class="span8">';
+                    echo '<iframe class="pull-right visible-desktop" src="http://www.hulu.com/embed.html?eid=' . $eid . '" width="640px" height="360px" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>';
+                    echo '<iframe class="pull-right visible-tablet" src="http://www.hulu.com/embed.html?eid=' . $eid . '" width="320px" height="240px" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>';
+                    echo '</div></div></div>';
                 }
             }
             exit;
--- index.php
+++ index.php
@@ -5,43 +5,59 @@ function videoSpinner() {
 	$xml = simplexml_load_file("http://m.hulu.com/videos?dp_id=hulu&order_by=desc&limit=%d&page=1&total=1");
 	$totalVideos = $xml->total_count;
 	$totalVideos = intval($totalVideos);
-	$GLOBALS['huluRandom'] = mt_rand(2, $totalVideos);
-	echo $GLOBALS['huluRandom'];
+	$huluRandom = mt_rand(2, $totalVideos);
+	echo $huluRandom;
 }
 ?>	
 <!DOCTYPE html>
+<!-- Thanks: http://adammagana.com/ and Hulu -->
 <html lang='en'>
 	<head>
 		<meta charset="utf-8">
-		<title>HuluRandom - Randomly pick something to watch on Hulu</title>
+		<title>hulurandom - Randomly find something to watch on Hulu</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='assets/application.css' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
 		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     	<!--[if lt IE 9]>
       	<script src="assets/javascripts/html5shiv.js"></script>
     	<![endif]-->
 	</head>
 	<body>
-		<h1><i class="icon-search"></i>HuluRandom</h1>
-		<h2>Randomly pick something to watch on Hulu</h2>
-	<hr>
-    <section>
-        <form action="api.php?method=getVideos" method="GET">
-            <input type="text" name="limit" value="1"/>
-            <input type="text" name="order_by" value="name%20desc"/>
-            <!--
-            <input type="text" name="order_by" value="name%20asc"/>
-            -->
-            <input type="text" name="page" value="<?php videoSpinner(); ?>"/>
-            <input type="text" name="total" value="0"/>
-        	
-        	
-            <button class="btn btn-success" type="submit">Get It! <span class="loader hide">combobulating...</span></button>
-        </form>
-        <h3>Response:</h3>
-        <div class="response_container">No data yet...</div>
-    </section>
-		<script src="assets/jquery.min.js" type='text/javascript'></script>
-		<script src='assets/application.min.js' type='text/javascript'></script>
+		<header class="mobile-padding">
+			<div class="container">
+				<div class="row">
+					<h1 class="span12"><span>hulu</span>random</h1>
+				</div>
+			</div>
+		</header>
+		<div class="leader mobile-padding">
+			<section>
+					<h2>Randomly find something to watch on Hulu</h2>
+        			<form action="api.php?method=getVideos" method="GET">
+            			<input type="hidden" name="limit" value="1"/>
+            			<input type="hidden" name="order_by" value="name%20desc"/>
+            			<!--
+            			<input type="text" name="order_by" value="name%20asc"/>
+            			-->
+            			<input type="hidden" name="page" value="<?php videoSpinner(); ?>"/>
+            			<input type="hidden" name="total" value="0"/>
+            			<button class="btn btn-success btn-large" type="submit">Find Something! <span class="loader hide"><img src="assets/images/ajax-loader.gif"></span></button>
+        			</form>
+        	<div class="response_container">
+        	</div>
+    		</section>
+    </div>
+    <footer>
+    	<div class="container">
+    		<div class="row">
+    			<div class="span12">
+    				<p>This site was designed to make evenings watcing TV more interesting, and is not affiliated whatsoever with Hulu.</p>
+    			</div>
+    		</div>
+    	</div>
+    </footer>
+	<script src="assets/jquery.min.js" type='text/javascript'></script>
+	<script src='assets/application.min.js' type='text/javascript'></script>
 	</body>
 </html>
